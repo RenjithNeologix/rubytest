a5206e755a1cfc929edfaf1203aac5945adc663e
RenjithNeologix <renjith.m@nlindia.com>
2016-11-05 23:14:12 +0530

    First commit

---
+++ dev/mysql_test/test.rb
@@ -0,0 +1,7 @@
+require 'mysql2'
+client = Mysql2::Client.new(host:'localhost',username:'root',password:'pass')
+sql = 'select NOW() as timestamp'
+result = client.query(sql)
+result.each do |row|
+ puts row['timestamp']
+end
